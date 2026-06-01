8c319f4cea7f18791e035844eb0fce7953d6c01f
montassar95 <montassar.trabelsi95@gmail.com>
2026-06-01 20:28:14 +0200

    feat: 20:27

--- src/main/java/tn/bensalah/immo/config/SecurityConfig.java
+++ src/main/java/tn/bensalah/immo/config/SecurityConfig.java
@@ -61,16 +61,23 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowedOrigins(List.of(
-        "http://localhost:4200",
-        "https://ben-salah-services.netlify.app"
-      ));
-      config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+//      config.setAllowedOrigins(List.of(
+//        "http://localhost:4200",
+//        "https://ben-salah-services.netlify.app"
+//      ));
+//      config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+//      config.setAllowedHeaders(List.of("*"));
+//      config.setAllowCredentials(true);
+//
+//      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      source.registerCorsConfiguration("/api/**", config);
+      config.setAllowedOriginPatterns(List.of("*")); // ✅ tout le monde
+      config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
       config.setAllowedHeaders(List.of("*"));
       config.setAllowCredentials(true);
 
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      source.registerCorsConfiguration("/api/**", config);
+      source.registerCorsConfiguration("/**", config);
       return source;
     }
 
