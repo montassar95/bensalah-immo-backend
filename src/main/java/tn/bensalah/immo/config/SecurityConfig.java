0c5b882d7213580beab1e99c8e70f172eb3c078f
montassar95 <montassar.trabelsi95@gmail.com>
2026-06-01 20:15:06 +0200

    feat: config CROS

--- src/main/java/tn/bensalah/immo/config/SecurityConfig.java
+++ src/main/java/tn/bensalah/immo/config/SecurityConfig.java
@@ -60,16 +60,18 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-        return source -> {
-            UrlBasedCorsConfigurationSource s = new UrlBasedCorsConfigurationSource();
-            s.registerCorsConfiguration("/**", config);
-            return s.getCorsConfiguration(source);
-        };
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowedOrigins(List.of(
+        "http://localhost:4200",
+        "https://ben-salah-services.netlify.app"
+      ));
+      config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+      config.setAllowedHeaders(List.of("*"));
+      config.setAllowCredentials(true);
+
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/api/**", config);
+      return source;
     }
 
     @Bean
